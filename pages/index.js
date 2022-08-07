23086641e740a24f82ad2761cf71bdfe36413ff4
Mogy1945 <hatano14150@gmail.com>
2022-08-08 00:31:09 +0900

    Link to firebase & set blog function

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,76 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Layout, { siteTitle } from "./components/Layout";
 
 import Link from "next/link";
 import utilStyle from "../styles/utils.module.css";
-import { getPostsData } from "../lib/post";
 
-//SSGの場合
+import React, { useState, useEffect } from "react";
+import { db } from "../src/firebase";
+
 export async function getStaticProps() {
-  const allPostsData = getPostsData(); //id,title,date,thumbnail
-
+  const blogDocuments = [];
+  const ref = await db.collection("blog-documents").get();
+  ref.docs.map((doc) => {
+    const data = {
+      id: doc.id,
+      title: doc.data().title,
+      thumbnail: doc.data().thumbnail,
+      date: doc.data().date,
+      article: doc.data().article,
+    };
+    blogDocuments.push(data);
+  });
   return {
     props: {
-      allPostsData,
+      blogDocuments,
     },
   };
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({ blogDocuments }) {
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  const [thumbnail, setThumbnail] = useState("");
+  const [article, setArticle] = useState("");
+
+  const [documents, setDocuments] = useState([
+    { id: "", title: "", thumbnail: "", date: "", article: "" },
+  ]);
+  useEffect(() => {
+    const unSub = db.collection("blog-documents").onSnapshot((snapshot) => {
+      setDocuments(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          title: doc.data().title,
+          thumbnail: doc.data().thumbnail,
+          date: doc.data().date,
+          article: doc.data().article,
+        }))
+      );
+    });
+
+    return () => unSub();
+  }, []);
+
+  const newArticle = () => {
+    db.collection("blog-documents").add({
+      title: title,
+      date: date,
+      thumbnail: thumbnail,
+      article: article,
+    });
+    setTitle("");
+    setDate("");
+    setThumbnail("");
+    setArticle("");
+  };
+
+  const deleteArticle = (e) => {
+    const deletTarget = e.currentTarget.dataset.docid;
+    db.collection("blog-documents").doc(deletTarget).delete();
+  };
+
   return (
     <>
       <Layout home>
@@ -28,26 +80,61 @@ export default function Home({ allPostsData }) {
         <section className={`${utilStyle.headingMd} `}>
           <p>React勉強中!日々の勉強などなどを記載していきます。</p>
         </section>
-
+        <div>
+          <p>タイトル</p>
+          <input
+            type="text"
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+          <p>日付</p>
+          <input
+            type="text"
+            onChange={(e) => {
+              setDate(e.target.value);
+            }}
+          />
+          <p>サムネイル画像のパス</p>
+          <input
+            type="text"
+            onChange={(e) => {
+              setThumbnail(e.target.value);
+            }}
+          />
+          <p>本文</p>
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            onChange={(e) => {
+              setArticle(e.target.value);
+            }}
+          ></textarea>
+          <button onClick={newArticle}>投稿する</button>
+        </div>
         <section className={`${utilStyle.headingMd} ${utilStyle.padding1px}`}>
           <h2>📝エンジニアのブログ</h2>
           <div className={styles.grid}>
-            {allPostsData.map(({ id, title, date, thumbnail }) => {
-              console.log(id, title, date, thumbnail);
+            {documents.map((document) => {
               return (
-                <article key={id}>
-                  <Link href={`/posts/${id}`}>
+                <article key={document.title}>
+                  <button data-docId={document.id} onClick={deleteArticle}>
+                    この記事を削除する
+                  </button>
+                  <Link href={`/posts/${document.id}`}>
                     <img
-                      src={`${thumbnail}`}
+                      src={`${document.thumbnail}`}
                       alt="サムネイル画像"
                       className={styles.thumbnailImage}
                     />
                   </Link>
-                  <Link href={`/posts/${id}`}>
-                    <a className={utilStyle.boldText}>{title}</a>
+                  <Link href={`/posts/${document.id}`}>
+                    <a className={utilStyle.boldText}>{document.title}</a>
                   </Link>
                   <br />
-                  <small className={utilStyle.lightText}>{date}</small>
+                  <small className={utilStyle.lightText}>{document.date}</small>
                 </article>
               );
             })}
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,42 +1,74 @@
 import Head from "next/head";
-import { getAllPostIds, getPostData } from "../../lib/post";
 import Layout from "../components/Layout";
 import utilStyles from "../../styles/utils.module.css";
+import { db } from "../../src/firebase";
+import { useRouter } from "next/router";
 
-export async function getStaticPaths() {
-  const paths = getAllPostIds();
-
-  return {
-    paths,
-    fallback: false, //pathsに含まれていないパスにアクセスすると404エラーとなる
-  };
-}
-
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
-
+export async function getStaticProps() {
+  const blogDocuments = [];
+  const ref = await db.collection("blog-documents").get();
+  ref.docs.map((doc) => {
+    const data = {
+      id: doc.id,
+      title: doc.data().title,
+      thumbnail: doc.data().thumbnail,
+      date: doc.data().date,
+      article: doc.data().article,
+    };
+    blogDocuments.push(data);
+  });
   return {
     props: {
-      postData,
+      blogDocuments,
     },
   };
 }
 
-export default function Post(postData) {
+export const getStaticPaths = async () => {
+  const blogDocuments = [];
+  const ref = await db.collection("blog-documents").get();
+  ref.docs.map((doc) => {
+    const data = {
+      id: doc.id,
+      title: doc.data().title,
+      thumbnail: doc.data().thumbnail,
+      date: doc.data().date,
+      article: doc.data().article,
+    };
+    blogDocuments.push(data);
+  });
+
+  const paths = blogDocuments.map((blogDocument) => ({
+    params: {
+      id: blogDocument.id.toString(),
+    },
+  }));
+  return { paths, fallback: false };
+};
+
+export default function Post(blogDocuments) {
+  const router = useRouter();
+  const urlId = router.query.id;
+  let countNum = 0;
+  let flgNum = 0;
+  const targetId = blogDocuments.blogDocuments.map((blogDocument) => {
+    countNum++;
+    if (blogDocument.id === urlId) {
+      flgNum = countNum;
+      return blogDocument;
+    }
+  });
+
+  const targetObj = targetId[flgNum - 1];
   return (
     <Layout>
       <Head>
-        <title>{postData.postData.title}</title>
+        <title>{targetObj.title}</title>
       </Head>
       <article>
-        <h1 className={utilStyles.headingX1}>{postData.postData.title}</h1>
-        <div className={utilStyles.lightText}>{postData.postData.date}</div>
-
-        <div
-          dangerouslySetInnerHTML={{
-            __html: postData.postData.blogContentHTML,
-          }}
-        />
+        <h1 className={utilStyles.headingX1}>{targetObj.title}</h1>
+        <div className={utilStyles.lightText}>{targetObj.date}</div>
+        <div>{targetObj.article}</div>
       </article>
     </Layout>
   );
