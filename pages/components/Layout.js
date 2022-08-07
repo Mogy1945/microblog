d61baf4aa93572c94a05d6c099f26e260bafe04f
Mogy1945 <hatano14150@gmail.com>
2022-08-07 13:50:37 +0900

    conplete md blog system

---
+++ pages/components/Layout.js
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import styles from "./layout.module.css";
+import utilStyles from "../../styles/utils.module.css";
+import Link from "next/link";
+
+const name = "もぎ";
+export const siteTitle = "Next.js blog";
+
+function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="favicon.ico" />
+      </Head>
+      <header className={styles.header}>
+        <img
+          src="/images/profile.png"
+          alt="プロフィール画像"
+          className={
+            home
+              ? `${utilStyles.borderCircle} ${styles.headerHomeImage}`
+              : `${utilStyles.borderCircle}`
+          }
+        />
+        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div>
+          <Link href="/">ホームへ戻る</Link>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Layout;
